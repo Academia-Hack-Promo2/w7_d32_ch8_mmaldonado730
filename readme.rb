7b7fc9a5dd6cb38d3de7554788c9f6f1bfe50a7b
marco <maldonmarco@gmail.com>
2015-04-10 14:40:37 -0430

    Palindrome

---
+++ readme.rb
@@ -0,0 +1,4 @@
+Palindrome
+El usuario suministra una palabra (ojo! es una palabra), el sistema determina si es Palindrome. http://en.wikipedia.org/wiki/Palindrome
+
+No se puede utilizar la función reverse de ruby.
